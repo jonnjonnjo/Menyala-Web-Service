76c3eaf33b5ac3ccad8efdefe2395dadd46b74cf
fawwazabrials <18221067@std.stei.itb.ac.id>
2024-06-09 15:02:20 +0700

    ci/cd: change port assignment

--- app.ts
+++ app.ts
@@ -5,30 +5,26 @@ import express from 'express';
 import router from './src/user';
 import cookieParser from 'cookie-parser';
 import { iotRouter } from './src/iot';
-import { Request,Response } from 'express';
-import cors from "cors"
+import { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = 7000;
+const PORT = parseInt(process.env.PORT!) || 3000;
+
 export const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors())
-
-//app.post('/iot', insertIot)  // This is for geting the data from the iot device
+app.use(cors());
 
 app.use('/iot', iotRouter);
 app.use('/user', router); // this is for user registration and login
 
-//app.get('/iot-data',authMiddleware, getWeeklyData)
-
-app.get("/",(req:Request,res:Response)=>{
+app.get('/', (req: Request, res: Response) => {
 	res.json({
-		message:"HELLO"
-	})
-})
-app.listen(port,"0.0.0.0", () => {
-	console.log('Listening on port ', port);
+		message: 'HELLO',
+	});
+});
+app.listen(PORT, '0.0.0.0', () => {
+	console.log('Listening on port ', PORT);
 });
